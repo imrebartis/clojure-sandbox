81a8fe6bdd31a9d8eff05e67a6961341f46307ef
imrebartis <imre.bartis@siili.com>
2017-10-02 19:58:05 +0300

    first commit

---
+++ one.clj
@@ -0,0 +1,21 @@
+(println "Hello, world!")
+"h"
+100
+true
+
+(type 'a)
+
+(def a "aaaaa")
+
+(let [l "light"] (println (str "God said let there be " l)))
+
+(let [l "light"
+             d "darkness"]
+             (println (str "God said let there be " l))
+             (println (str "God also said let there be " d)))
+
+(let [l "light"
+            l_d (str l " and" " darkness")]
+            (println (str "God also said let there be " l_d)))
+
+
